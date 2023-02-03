a9f05129afe600b64f11591bbbd0b582d0e9d581
akshayjagtap1993 <akshayjagtap1993@gmail.com>
2023-02-03 15:32:06 +0530

    added default api

--- src/main/java/com/ecommerce/userservice/controller/UserController.java
+++ src/main/java/com/ecommerce/userservice/controller/UserController.java
@@ -18,6 +18,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/")
+    public String getDefault() {
+        return "Application user-service is up";
+    }
+
     @GetMapping("/users")
     public ResponseEntity<BaseResponse> getAllUsers() {
         var usersList = userService.getAllUsers();
